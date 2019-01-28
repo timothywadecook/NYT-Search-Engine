2a8f590dbf142c5870fd0fab65f75a7cccf6dc47
Timothy Cook <tim.wade.cook@gmail.com>
2019-01-28 13:50:50 -0500

    search no longer appends and default thumbnail picture set to nyt logo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const apiKey = "nqjPt1B0olMlGRLvicI7VQuw9ub082GM";
 $("#search").on("click", function(e) {
   e.preventDefault();
   console.log("searching..");
+  $('#results').empty();
   queryApi();
 });
 
@@ -22,7 +23,7 @@ const render = function(data) {
     // console.log(doc)
     $("#results").append(`
         <li class="media my-4">
-            <img src="https://static01.nyt.com/${doc.multimedia[2].url}" class="mr-3" alt="Article Image">
+            <img height="75" width="75" src="${doc.multimedia[2] ? 'https://static01.nyt.com/' + doc.multimedia[2].url : 'nyt.jpg'}" class="mr-3" alt="Article Image">
             <div class="media-body">
                 <h5 class="mt-0">
                 ${doc.headline.main}
@@ -46,6 +47,13 @@ const queryApi = function() {
     url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&begin_date=${begin_date}&end_date=${end_date}&api-key=${apiKey}`,
     method: "GET"
   }).then(function({ response }) {
-    render(response.docs);
+    switch (numRecords) {
+      case '1': render(response.docs.slice(0,1));
+      break;
+      case '5': render(response.docs.slice(0,5));
+      break;
+      case '10': render(response.docs);
+      break;
+    };
   });
 };
